db3fc137e02f5836f7fb87ad07964453d8b738af
Martin Halaj <m.halaj@websystem.sk>
2021-11-13 16:31:03 +0100

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,8 +29,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-
-    
     el: '#app',
     created() {
         window.Echo.channel('notification')
